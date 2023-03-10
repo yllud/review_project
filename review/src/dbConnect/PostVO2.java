c26dd7fa74508aaa90c6866e46323e8f1d1bea81
Yang Hyeon Jun <yang960920@naver.com>
2023-03-10 16:08:46 +0900

    POST Commit 1

---
+++ review/src/dbConnect/PostVO2.java
@@ -0,0 +1,62 @@
+package dbConnect;
+
+public class PostVO2 {
+	private int post_no;
+	private String title;
+	private String content;
+	private String writer;
+	private int score;
+	private String write_date;
+	private String place_code;
+	public int getPost_no() {
+		return post_no;
+	}
+	public void setPost_no(int post_no) {
+		this.post_no = post_no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getWrite_date() {
+		return write_date;
+	}
+	public void setWrite_date(String write_date) {
+		this.write_date = write_date;
+	}
+	public String getPlace_code() {
+		return place_code;
+	}
+	public void setPlace_code(String place_code) {
+		this.place_code = place_code;
+	}
+
+@Override
+	public String toString() {
+		return "PostVO2 [post_no=" + post_no + ", title=" + title + ", content=" + content + ", writer=" + writer
+				+ ", score=" + score + ", write_date=" + write_date + ", place_code=" + place_code + "]";
+	}
+	
+	
+	
+}
